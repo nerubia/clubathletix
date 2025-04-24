1317d62f61b774b2306c06d4dadf4b8eb4a90073
Gerard Rey <gerard@echiverri.net>
2025-04-24 14:49:41 -0700

    fix: id does not exist on params

--- src/app/(app)/members/[id]/page.tsx
+++ src/app/(app)/members/[id]/page.tsx
@@ -22,7 +22,7 @@ export async function generateMetadata({ params }: { params: Promise<{ id: strin
 
 export default async function Member({ params }: { params: Promise<{ id: string }> }) {
   const {id} = await params
-  let member = await getMember(params.id)
+  let member = await getMember(id)
 
   if (!member) {
     notFound()
