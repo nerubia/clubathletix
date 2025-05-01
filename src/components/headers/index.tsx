f0c90eec7cd043c8b6fecc21411da6c696bfb7da
Gerard Rey <gerard@echiverri.net>
2025-05-01 15:41:09 -0700

    fix: header logo

--- src/components/headers/index.tsx
+++ src/components/headers/index.tsx
@@ -55,12 +55,12 @@ export default function HomePageHeader() {
           <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
+                <span className="sr-only">Progress Footy Football Academy Inc.</span>
                 <img
-                  alt=""
-                  src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                  className="h-8 w-auto"
-                />
+                alt=""
+                src="/clubs/pfa/logo.png"
+                className="h-12 w-auto"
+              />
               </a>
               <button
                 type="button"
