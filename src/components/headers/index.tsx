a96d2cefa0dce40956da23368140ad58b58bbeba
Gerard Rey <gerard@echiverri.net>
2025-05-01 18:30:16 -0700

    fix: mobile nav

--- src/components/headers/index.tsx
+++ src/components/headers/index.tsx
@@ -86,10 +86,10 @@ export default function HomePageHeader() {
                 </div>
                 <div className="py-6">
                   <a
-                    href="#"
+                    href="#signup"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                   >
-                    Log in
+                    Sign Up
                   </a>
                 </div>
               </div>
