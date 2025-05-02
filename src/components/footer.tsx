8a4a8e8dd2d36c1d2a93b79527ed51214b2a5fe6
Gerard Rey <gerard@echiverri.net>
2025-05-01 18:28:42 -0700

    fix: sponsor

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-900">
       <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
-        <div className="flex flex-col sm:flex-row justify-center gap-x-6 md:order-2">
+        <div className="flex justify-center gap-x-6 md:order-2">
           {navigation.map((item) => (
             <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-300">
               <span className="sr-only">{item.name}</span>
--- src/components/sponsors/index.tsx
+++ src/components/sponsors/index.tsx
@@ -5,9 +5,9 @@ export default function Sponsors() {
       <div className="bg-gray-900 py-24 sm:py-32" id="sponsors">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <h2 className="text-center text-lg/8 font-semibold text-white">Proudly sponsored and supported by</h2>
-          <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+          <div className="mx-auto mt-10 grid max-w-lg grid-cols-2 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
             
-            <Heading force="text-white" className="!font-bold text-center">CLUBATHLETIX</Heading>
+            <Heading force="text-white" className="!font-bold text-center text-sm">CLUBATHLETIX</Heading>
             <Heading force="text-white" className="!font-bold text-center">Nerubia</Heading>
             <img
               alt="Bird"
