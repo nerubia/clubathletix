c1f8816d2b73a94de458beb8c76c6ba1472c6ed6
Gerard Rey <gerard@echiverri.net>
2025-05-05 19:40:02 -0700

    fix(navigation.tsx): anchor error

--- src/components/headers/navigation.tsx
+++ src/components/headers/navigation.tsx
@@ -14,14 +14,14 @@ export default function Navigation({ backgroundColour }: { backgroundColour?: st
     return <header className={`absolute inset-x-0 top-0 z-50 ${backgroundColour || ''}`.trim()}>
     <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
       <div className="flex lg:flex-1">
-        <a href="/" className="-m-1.5 p-1.5">
+        <Link href="/" className="-m-1.5 p-1.5">
           <span className="sr-only">PF Academy</span>
           <img
             alt=""
             src="/clubs/pfa/logo.png"
             className="h-12 w-auto"
           />
-        </a>
+        </Link>
       </div>
       <div className="flex lg:hidden">
         <button
@@ -48,14 +48,14 @@ export default function Navigation({ backgroundColour }: { backgroundColour?: st
       <div className="fixed inset-0 z-50" />
       <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
         <div className="flex items-center justify-between">
-          <a href="/" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Progress Footy Football Academy Inc.</span>
             <img
             alt=""
             src="/clubs/pfa/logo.png"
             className="h-12 w-auto"
           />
-          </a>
+          </Link>
           <button
             type="button"
             onClick={() => setMobileMenuOpen(false)}
