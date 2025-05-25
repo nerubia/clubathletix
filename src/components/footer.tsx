f3ab5fa2d27e2e122998f635eb9e827e546c6d99
Gerard Rey <gerard@echiverri.net>
2025-05-25 12:55:27 -0700

    style(footer): responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -21,14 +21,14 @@ export default function Footer({'data-org': organization}: {
   return (
     <footer className="bg-gray-900">
       <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
-        <div className="flex justify-center gap-x-6 md:order-2">
+        <div className="flex justify-center gap-2 md:order-2 flex-wrap">
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-300">
+            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-300 w-6">
               <span className="sr-only">{item.name}</span>
               <item.icon aria-hidden="true" className="size-6" />
             </a>
           ))}
-          <p className="mt-8 text-center text-sm/6 text-gray-400 md:order-1 md:mt-0">
+          <p className="mt-8 text-center text-xs/6 text-gray-400 md:order-1 md:mt-0 w-full md:w-auto">
           Powered by ClubAthletix</p>
         </div>
         <p className="mt-8 text-center text-sm/6 text-gray-400 md:order-1 md:mt-0">
