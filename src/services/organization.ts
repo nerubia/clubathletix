f9c1cb2ea29d1d9568fc2c4e94f1297d766dda69
Gerard Rey <gerard@echiverri.net>
2025-05-24 20:42:23 -0700

    feat(clubathletix): video

--- src/services/organization.ts
+++ src/services/organization.ts
@@ -4,7 +4,7 @@ export async function getOrganizationByDomain(domain: string) {
     const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!)
     const [hostname] = domain.split(':')
     
-    const { data: records, error } = await supabase.from('domains').select('id, name, organizations (id, name, short_name, logo_url)').eq('name', domain.split(':')[0])
+    const { data: records, error } = await supabase.from('domains').select('id, name, organizations (id, name, short_name, logo_url)').eq('name', hostname)
     
     const data = records?.pop()?.organizations
     if (data) {
