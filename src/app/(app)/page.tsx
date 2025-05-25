3e423df43efdf84d2aaa3c33c8146e4543b576ab
Gerard Rey <gerard@echiverri.net>
2025-05-24 19:05:34 -0700

    feat(page.tsx): custom for clubathletix

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,12 +1,13 @@
-import Footer from '@/components/footer'
 import HomePageHeader from '@/components/headers'
 import SignupSection from '@/components/sign-up-sections/summer'
-import Sponsors from '@/components/sponsors'
-import Testimonials from '@/components/testimonials'
 import TestimonialGrid from '@/components/testimonials/grid'
 import { getOrganizationTestimonials } from '../api/testimonial/service'
+import { headers } from 'next/headers'
+import VideosPage from './videos/page'
 
 export default async function Page() {
+    const hdr = await headers()
+    if(hdr.get('host') === 'clubathletix.com') return <VideosPage />
   function getAddress() {
     let address = 'Cambridge Soccer Park - 6067 - 150 Street, Surrey BC'
     if (new Date().getMonth() < 7) address = 'Goldstone Park'
--- src/app/(app)/videos/page.tsx
+++ src/app/(app)/videos/page.tsx
@@ -2,46 +2,47 @@ import { CheckIcon } from '@heroicons/react/20/solid'
 
 const tiers = [
   {
-    name: 'Record',
-    id: 'record',
+    name: 'Player Match Highlight',
+    id: 'highlight',
     href: 'whatsapp:+12367771283',
-    price: 'CA$79',
+    price: 'CA$19',
     unit: '/match',
-    description: 'HD-quality footage using professional AI-assisted cameras, with elevated tripod for maximum coverage.',
-    features: ['Full shareable link', 'Event Tagging & Match Highlights' , 'View your video 6 - 12 hours after the match.'],
+    description: 'HD-quality footage using professional AI-assisted cameras for your player (based on position).',
+    features: ['Goalie moments (saves / assists)' , 'Defensive moments (successful tackles)' , 'Midfielders moments (crosses, passes, successful plays)', 'Attackers moments (goals, assists, crosses)', 'View your video 4 - 12 hours after the match.*'],
   },
   {
-    name: 'Evo+',
-    id: 'evolutionplus',
-    href: 'whatsapp:+12367771283',
-    price: 'CA$129',
-    unit: '/match',
-    description: 'Get the full match plus a curated team highlight reel.',
-    features: [
-      'Everything in the "Record" package',
-      'Team highlight reels',
-      'Analytics (possession ratio, passing rate, etc.)',
-      'Get a shareable highlight reel posted into Instagram for your team',
-      'Downloadable highlight reel',
-    ],
-  },
-  {
-    name: 'Player Evo+',
-    id: 'playerevoplus',
+    name: 'Player Evo Season Highlights',
+    id: 'seasonplus',
     href: 'whatsapp:+12367771283',
     price: 'CA$99',
-    unit: '/season',
-    description: 'Get the full match plus a curated team highlight reel.',
+    unit: '/5 matches',
+    description: 'Perfect for college recruitment or personal keepsakes, we create highlight compilations of 5 of your best matches.',
     features: [
-      'Everything in the "Record" package',
-      'Player Evolution Highlight Reel (45 seconds)',
-      'Get a highlight reel posted into Instagram for you to easily share',
-      'Downloadable highlight reel',
+      'Player Match Highlights of 5 matches',
+      'With our ClubAthletix AI software suite, stich everything up to create a 45-second video.',
+      'Downloadable video, plus',
+      'Featured in ClubAthletix Instagram page for easy sharing with scouts and coaches',
     ],
   },
+  {
+    name: 'Player Evo Highlight',
+    id: 'evohighlight',
+    href: 'whatsapp:+12367771283',
+    price: 'CA$39',
+    unit: '/match',
+    description: 'Get those studio grade shots and get a REEL highlight for the ages!',
+    features: [
+      'Player Match Highlights, plus',
+      'We get additional close up / angled shots of your player in high definition.',
+      'We process and curate everything up to create a 45-second reel (portrait).',
+      'Featured in ClubAthletix Instagram page for easy sharing with scouts and coaches',
+    ],
+    video: ''
+  },
+  
 ]
 
-export default function Example() {
+export default function VideosPage() {
   return (
     <div className="isolate overflow-hidden bg-gray-900">
       <div className="mx-auto max-w-7xl px-6 pt-24 pb-96 text-center sm:pt-32 lg:px-8">
@@ -76,7 +77,7 @@ export default function Example() {
               {tiers.map((tier) => (
                 <div
                   key={tier.id}
-                  className={`flex justify-between rounded-3xl bg-white shadow-xl ring-1 ring-gray-900/10  ${tier.id === 'playerevoplus' ? 'col-span-2' : 'flex-col p-8 sm:p-10'}`}
+                  className={`flex rounded-3xl bg-white shadow-xl ring-1 ring-gray-900/10  ${tier.id === 'playerevoplus' ? 'col-span-2' : 'justify-between flex-col p-8 sm:p-10'}`}
                 >
                   <div className={`${tier.id === 'playerevoplus' ? 'p-8 ' : ''} flex flex-col`}>
                     <h3 id={tier.id} className="text-base/7 font-semibold text-red-600">
@@ -95,15 +96,23 @@ export default function Example() {
                         </li>
                       ))}
                     </ul>
-                    <div className='flex-1' />
-                    <a
+                    <div className='flex-1 grow' />
+                    {tier.id === 'playerevoplus' && <a
                         href={tier.href}
                         aria-describedby={tier.id}
                         className="mt-8 block rounded-md bg-red-600 px-3.5 py-2 text-center text-sm/6 font-semibold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                     >
                         Book (1st come 1st serve basis)
-                    </a>
+                    </a>}
                   </div>
+                    {tier.id !== 'playerevoplus' && <div className='flex-1 grow' />}
+                    {tier.id !== 'playerevoplus' && <a
+                        href={tier.href}
+                        aria-describedby={tier.id}
+                        className="mt-8 block rounded-md bg-red-600 px-3.5 py-2 text-center text-sm/6 font-semibold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                    >
+                        Book (1st come 1st serve basis)
+                    </a>}
 
                     {tier.id === 'playerevoplus' && <div className="mt-4 flex items-baseline gap-x-2">
                         <div className="relative">
@@ -113,10 +122,14 @@ export default function Example() {
                     </div>}
                 </div>
               ))}
+              <div className="relative">
+                            <video autoPlay loop playsInline muted className="overflow-hidden rounded-2xl">
+                            <source src="/videos/reel.mp4" type="video/mp4" />Your browser does not support the video tag.</video>    
+                        </div>
               <div className="flex flex-col items-start gap-x-8 gap-y-6 sm:gap-y-10 lg:col-span-2 lg:flex-row lg:items-center">
                 <div className="lg:min-w-0 lg:flex-1">
                   <h3 className="text-base/7 font-semibold text-red-600">How it works</h3>
-                  <ol className='list-decimal'>
+                  <ol className='list-decimal ml-4'>
 
                     <li className="mt-1 text-base/7 text-gray-600">
                         <strong>Book a Match</strong><br/>
@@ -128,13 +141,13 @@ export default function Example() {
                     </li>
                     <li className="mt-1 text-base/7 text-gray-600">
                         <strong>You Receive Edited Video</strong><br/>
-                        Get your match footage (3 - 8 hrs) + any selected highlight packages online in a matter of days.
+                        Get your match footage (4 - 12 hrs) + any selected highlight packages online in a matter of days.
                     </li>
                   </ol>
 
                 </div>
 
-                  <div className="relative w-full lg:w-1/2">
+                  <div className="relative w-full lg:w-1/2 xl:pl-4">
                     <video width="100%" autoPlay loop playsInline muted className="overflow-hidden rounded-2xl">
                         <source src="/videos/go.mp4" type="video/mp4" height="240" />Your browser does not support the video tag.
                     </video>
