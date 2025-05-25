e397ef9da19639c9a3b0a4c43889ff99d5036b41
Gerard Rey <gerard@echiverri.net>
2025-05-25 11:19:46 -0700

    fix(clubathletix): single quote

--- src/components/heroes/clubathletix.tsx
+++ src/components/heroes/clubathletix.tsx
@@ -49,17 +49,17 @@ export default function HomeHeroSection(p: {
       </div>
       <div className="mx-auto max-w-7xl px-6 pt-10 pb-24 sm:pb-32 lg:flex lg:px-8 lg:py-40">
         <div className="mx-auto max-w-2xl shrink-0 lg:mx-0 lg:pt-8">
-          <div className="mt-24 sm:mt-32 lg:mt-16">
+          {/* <div className="mt-24 sm:mt-32 lg:mt-16">
             <a href="#" className="inline-flex space-x-6">
               <span className="rounded-full bg-rose-500/10 px-3 py-1 text-sm/6 font-semibold text-rose-400 ring-1 ring-rose-500/20 ring-inset">
-                What's new
+                What&rsquo;s new
               </span>
               <span className="inline-flex items-center space-x-2 text-sm/6 font-medium text-gray-300">
                 <span>Just shipped v1.0</span>
                 <ChevronRightIcon aria-hidden="true" className="size-5 text-gray-500" />
               </span>
             </a>
-          </div>
+          </div> */}
           <h1 className="mt-10 text-5xl font-semibold tracking-tight text-pretty text-white sm:text-7xl">
             Player Evolution Platform
           </h1>
