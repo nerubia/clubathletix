d6845aca2428da990c02028e98b4a49f142c4187
Gerard Rey <gerard@echiverri.net>
2025-07-09 10:34:39 -0700

    feat: slack

--- src/app/api/slack/route.ts
+++ src/app/api/slack/route.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
     const data = await req.formData()
-    console.log(data);
+    
+    console.log(data.get('actions'));
     return NextResponse.json({
         message: "Slack integration is not available yet.",
     })
