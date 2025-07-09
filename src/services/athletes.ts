7c3663e3b8e67f8a79bfd95b4e8983d88b087479
Gerard Rey <gerard@echiverri.net>
2025-07-09 15:30:28 -0700

    feat: slack

--- src/services/athletes.ts
+++ src/services/athletes.ts
@@ -25,3 +25,28 @@ export async function getAthleteUrl(id: string): Promise<Database['public']['Tab
         console.error(e)
     }
 }
+
+export async function getAthleteViaSlack(username: string): Promise<Database['public']['Tables']['slack_users']['Row'] | undefined> {
+    const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!)
+
+    try {
+
+        const { data, error } =  await supabase
+            .from('slack_users')
+            .select(`
+                username,
+                athletes (full_name, id)`)
+            .eq('username', username).single()
+
+        if (error) throw error
+        
+        if (data) {
+            return data as unknown as Database['public']['Tables']['slack_users']['Row']
+        } else {
+            console.warn(`No Slack record found for ID: ${username}`)
+            return undefined
+        }
+    } catch (e) {
+        console.error(e)
+    }
+}
