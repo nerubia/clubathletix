5a95182510ebfbe2162eb4934ffe8d241604785d
Gerard Rey <gerard@echiverri.net>
2025-07-09 10:53:26 -0700

    feat: slack

--- src/app/api/slack/route.ts
+++ src/app/api/slack/route.ts
@@ -14,15 +14,20 @@ export async function POST(req: NextRequest) {
         },
         actions: {
             action_id: string;
-            text: {
-                value: string;
-            };
+            value: string;
+            action_ts: number;
         }[]
     }
-    console.log(JSON.stringify({
-        user: user.name,
-        answer: actions.pop(),
-    }, null, 2));
+    const action = actions.pop();
+    if (action) {
+        const answer = action.value;
+        const timestamp = new Date(action.action_ts * 1000);
+        console.log(JSON.stringify({
+            user: user.name,
+            answer,
+            timestamp,
+        }, null, 2));
+    }
 
     return NextResponse.json({
         message: "Slack integration is not available yet.",
