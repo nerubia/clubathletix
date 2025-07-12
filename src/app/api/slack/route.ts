ced19f024d339e1d8affe9a5b0f9164c1e901179
Gerard Rey <gerard@echiverri.net>
2025-07-12 08:56:17 -0700

    feat(api): slack

--- src/app/api/slack/route.ts
+++ src/app/api/slack/route.ts
@@ -8,108 +8,112 @@ export async function POST(req: NextRequest) {
 	console.log('Received request to /api/slack/route.ts', hdr.get('content-type'));
 	const data = await req.formData();
 	try {
+		console.log('Received data for /api/slack/route.ts', data);
 		const { payload } = Object.fromEntries(data);
-		const {
-			user,
-			actions,
-			message,
-			channel,
-			container: { channel_id },
-		} = JSON.parse(payload as unknown as string) as unknown as {
-			response_url: string;
-			user: {
-				id: string;
-				username: string;
-				name: string;
-			};
-			actions: {
-				action_id: string;
-				value: string;
-				action_ts: number;
-			}[];
-			message: {
-				ts: number;
-			};
-			channel: {
-				id: string;
-				name: string;
-			};
-			container: {
-				channel_id: string;
-			};
-		};
-
-		const thread_ts = message.ts;
-		const action = actions.pop();
-		const channels = await getSlackChannels();
-		const replyInChannel = channels.find((c) => c.id === channel_id || c.name === channel.name);
-		const slackUser = await getSlackUserProfile(user.id);
-		if (action) {
-			const answer = action.value;
+		if (payload) {
 			const {
-				user: { profile },
-			} = slackUser || { user: { profile: { email: '' } } };
-			if (profile.email && replyInChannel) {
-				const customer = await getAthleteViaEmail(profile.email);
-				const { athletes, email, phone, full_name, ...record } = customer || { athletes: [] };
-				const [from, to] = replyInChannel.name
-					.split('-')
-					.map((s) => Number(s.trim()))
-					.filter(Boolean);
+				user,
+				actions,
+				message,
+				channel,
+				container: { channel_id },
+			} = JSON.parse(payload as unknown as string) as unknown as {
+				response_url: string;
+				user: {
+					id: string;
+					username: string;
+					name: string;
+				};
+				actions: {
+					action_id: string;
+					value: string;
+					action_ts: number;
+				}[];
+				message: {
+					ts: number;
+				};
+				channel: {
+					id: string;
+					name: string;
+				};
+				container: {
+					channel_id: string;
+				};
+			};
 
-				let applicablePlayers: string[] = (
-					!from ? athletes.map((athlete) => athlete.full_name.split(',').pop()?.trim() || '') : []
-				).filter(Boolean);
-				if (from && to) {
-					applicablePlayers = athletes
-						.filter((athlete) => {
-							const [year] = athlete.date_of_birth.split('-').map(Number);
-							return year >= from && year <= to;
-						})
-						.map((athlete) => athlete.full_name.split(',').pop()?.trim() || '');
+			const thread_ts = message.ts;
+			const action = actions.pop();
+			const channels = await getSlackChannels();
+			const replyInChannel = channels.find((c) => c.id === channel_id || c.name === channel.name);
+			const slackUser = await getSlackUserProfile(user.id);
+			if (action) {
+				const answer = action.value;
+				const {
+					user: { profile },
+				} = slackUser || { user: { profile: { email: '' } } };
+				if (profile.email && replyInChannel) {
+					const customer = await getAthleteViaEmail(profile.email);
+					const { athletes, email, phone, full_name, ...record } = customer || { athletes: [] };
+					const [from, to] = replyInChannel.name
+						.split('-')
+						.map((s) => Number(s.trim()))
+						.filter(Boolean);
+
+					let applicablePlayers: string[] = (
+						!from ? athletes.map((athlete) => athlete.full_name.split(',').pop()?.trim() || '') : []
+					).filter(Boolean);
+					if (from && to) {
+						applicablePlayers = athletes
+							.filter((athlete) => {
+								const [year] = athlete.date_of_birth.split('-').map(Number);
+								return year >= from && year <= to;
+							})
+							.map((athlete) => athlete.full_name.split(',').pop()?.trim() || '');
+					}
+
+					if (applicablePlayers && applicablePlayers.length) {
+						const results = await submitSlackRequest('chat.postMessage', {
+							channel: replyInChannel.id,
+							text: applicablePlayers
+								.map((name) => `:${answer === 'yes' ? 'white_check_mark' : 'x'}: *${name}*`)
+								.join('\n'),
+							thread_ts,
+							mrkdwn: true,
+							reply_broadcast: true,
+							username: applicablePlayers.join(' • '),
+						});
+
+						// const xhr = await fetch(response_url, {
+						//     method: 'POST',
+						//     headers: {
+						//         'Content-Type': 'application/json',
+						//     },
+						//     body: JSON.stringify({
+						//         text: applicablePlayers.map(name => `:${answer === 'yes' ? 'white_check_mark' : 'x'}: *${name}*`).join('\n'),
+						//         response_type: 'in_channel',
+						//         replace_original: false,
+						//         thread_ts,
+						//         mrkdwn: true,
+						//         name: "heart",
+						//         username: applicablePlayers.join(' • '),
+						//     }),
+						// });
+						// return NextResponse.json({status: xhr.status, statusText: xhr.statusText}, {
+						//     headers: { 'Content-Type': 'application/json' },
+						//     status: xhr.status,
+						//     statusText: xhr.statusText,
+						// });
+						console.log(results);
+						return NextResponse.json({
+							message: 'Completed.',
+							results,
+						});
+					}
 				}
-
-				if (applicablePlayers && applicablePlayers.length) {
-					const results = await submitSlackRequest('chat.postMessage', {
-						channel: replyInChannel.id,
-						text: applicablePlayers
-							.map((name) => `:${answer === 'yes' ? 'white_check_mark' : 'x'}: *${name}*`)
-							.join('\n'),
-						thread_ts,
-						mrkdwn: true,
-						reply_broadcast: true,
-						username: applicablePlayers.join(' • '),
-					});
-
-					// const xhr = await fetch(response_url, {
-					//     method: 'POST',
-					//     headers: {
-					//         'Content-Type': 'application/json',
-					//     },
-					//     body: JSON.stringify({
-					//         text: applicablePlayers.map(name => `:${answer === 'yes' ? 'white_check_mark' : 'x'}: *${name}*`).join('\n'),
-					//         response_type: 'in_channel',
-					//         replace_original: false,
-					//         thread_ts,
-					//         mrkdwn: true,
-					//         name: "heart",
-					//         username: applicablePlayers.join(' • '),
-					//     }),
-					// });
-					// return NextResponse.json({status: xhr.status, statusText: xhr.statusText}, {
-					//     headers: { 'Content-Type': 'application/json' },
-					//     status: xhr.status,
-					//     statusText: xhr.statusText,
-					// });
-					console.log(results);
-					return NextResponse.json({
-						message: 'Completed.',
-						results,
-					});
-				}
+				return NextResponse.json({ message: 'No email found in Slack user profile.' });
 			}
-			return NextResponse.json({ message: 'No email found in Slack user profile.' });
 		}
+
 		// const athletes = await getCustomerByEmail(email);
 		// console.log(JSON.stringify({athletes}, null, 2))
 
