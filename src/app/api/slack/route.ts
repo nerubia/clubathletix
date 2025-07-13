ce4e5ad0f8f33d526a572a1b8bccee1572477b12
Gerard Rey <gerard@echiverri.net>
2025-07-12 17:07:02 -0700

    feat(api): slack

--- src/app/api/slack/route.ts
+++ src/app/api/slack/route.ts
@@ -62,7 +62,7 @@ export async function POST(req: NextRequest) {
                             mrkdwn: true,
                             name: "heart"
                         }),
-                    })];
+                    })]);
                 }
             }
 		}
